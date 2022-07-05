5aef12b51c1c96f224173f95274ee8a24e9da2f2
krisjwood <kriswd0@gmail.com>
2022-07-05 21:06:09 +1200

    more content, bitcoin api call, remove me

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let bitcoinJSON = null;
+
 function copy() {
     navigator.clipboard.writeText("wget krisjwood.github.io/kris_wood_cv.pdf");
     document.getElementById("wget-snippet").innerHTML = 'Copied to clipboard!'
@@ -17,4 +19,29 @@ function love() {
     setTimeout(function() { 
         snackbar.className = snackbar.className.replace("show", ""); 
     }, 2000);
+}
+
+async function getBitcoinPrice() {
+    fetch("https://rest.coinapi.io/v1/exchangerate/GBP/BTC", {
+        method: "GET",
+        headers: {
+            "X-CoinAPI-Key": "31CDDF2F-330D-4697-A350-779B920463D0"
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        bitcoinJSON = json
+    })
+    .catch(err => console.error(err))
+}
+
+function openBitcoinModal() {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("modalFX").innerHTML = `£120k = &#x20BF;${(bitcoinJSON.rate * 120000).toFixed(2)}` ;
+    document.getElementById("modalFXtime").innerHTML = `${Date(bitcoinJSON.time)}` ;
+    
+}
+
+function closeBitcoinModal() {
+    document.getElementById("myModal").style.display = "none";
 }
